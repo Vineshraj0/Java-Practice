821a0c32256ce71c03685888fc2d7404db61d82a
Vinesh Raj <vinesh9562673788@gmail.com>
2022-11-26 07:18:00 +0530

    java

--- palindrome.java
+++ palindrome.java
@@ -1,10 +1,9 @@
 import java.util.*;
-
 public class palindrome{
 public static void main(String args[]){
 Scanner sc=new Scanner(System.in);
 int n,i,flag,n1,n2;
-System.out.println("Enter the limits: ");
+System.out.print("Enter the limits: ");
 n1=sc.nextInt();
 n2=sc.nextInt();
 for(n=n1;n<n2;n++)
@@ -16,16 +15,10 @@ for(i=2;i<n/2;i++)
 	{
 		flag=1;
 		break;
-	}
-	
-		
-		
+	}	
 }
-
 if(flag==0)
 	System.out.println(n);
-
-
 }
 }
 }
